532bbd2cc0dd6c69062c3ac06446ca4728dc7524
enRamirez <ricky.ramirez125042@gmail.com>
2023-08-13 21:54:11 -0500

    Test

--- sql/connection.js
+++ sql/connection.js
@@ -31,26 +31,3 @@ module.exports = connection;
 
 
 
-// class based connectoin
-// class Connection {
-//   constructor() {
-//     if (!this.pool) {
-//       console.log('creating connection...')
-//       this.pool = mysql.createPool({
-//         connectionLimit: 100,
-//         host: 'process.env.DB_HOST',
-//         user: 'process.env.DB_USER',
-//         password: 'process.env.DB_PWD',
-//         database: 'process.env.DB_NAME'
-//       })
-
-//       return this.pool
-//     }
-
-//     return this.pool
-//   }
-// }
-
-// const instance = new Connection()
-
-// module.exports = instance;
